6be2fadc12a52381df2f9cffb1261457d19141a7
Juan Aguilar <juan-carlos.aguilar@atos.net>
2022-02-21 16:20:14 -0600

    Logical OR & AND activities

--- main.js
+++ main.js
@@ -1,34 +1,7 @@
-//console.log(test);
-//var test = "test";
-//let test = "test";
-//const test = "test";
+const test = 5 || 6;
 
-// test();
+const sample = 0 || 20;
 
-// function test() {
-//     console.log("It is a function");
-// }
+console.log(test);
 
-// function test() {
-//     var nice = "nice";
-// }
-
-// console.log(nice);
-
-// if(true) {
-//     var test = "test";
-// }
-
-// console.log(test);
-
-// for (let i = 0; i < 3; i++) {
-//     setTimeout(() => {
-//         alert(i);
-//     }, 500)
-// }
-
-for(let i = 0; i < 3; i++) {
-    (function test(o){
-        alert(o);
-    })(i)
-}
+console.log(sample);

d66462a291ade031c31c031af88d491c882f6d3f
Juan Aguilar <juan-carlos.aguilar@atos.net>
2022-02-22 16:42:49 -0600

    State - Hands On

--- react-udemy-tutorial/src/App.tsx
+++ react-udemy-tutorial/src/App.tsx
@@ -1,14 +1,13 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import {Button} from './components/Button'
+import CounterManagement from './components/CounterManagement';
 
 function App() {
   return (
     <>
       <h1>My App</h1>
-      <Button type="primary">Primary</Button>
-      <Button>Default</Button>
+      <CounterManagement ownerName="Carlos" />
     </>
   );
 }
---
+++ react-udemy-tutorial/src/components/CounterManagement/interface.ts
@@ -0,0 +1,6 @@
+export interface CounterManagementProps {
+    ownerName: string;
+}
+export interface CounterManagementState {
+    counter: number;
+}

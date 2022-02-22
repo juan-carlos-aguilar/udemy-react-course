396f79ead1011657df922a17dd780a34b39bfccd
Juan Aguilar <juan-carlos.aguilar@atos.net>
2022-02-22 10:34:32 -0600

    Spread operators, Rest parameter, Destructuring

--- main.js
+++ main.js
@@ -1,27 +1,44 @@
-// const orig = {
-//     test: 'test',
-//     userDetails: {
-//         address: {
-//             province: 'Province',
-//             country: 'country'
-//         },
-//         name: 'myObject'
-//     },
-//     myFn: () => {},
-//     undef: undefined,
+// function sum(...thisArgs) {
+//     return thisArgs.reduce(function(previous, current){
+//         return previous + current;
+//     })
 // }
 
-const orig = ['avocado', 'apple', {name: 'strawberry'}];
+// function sum(...thisArgs) {
+//     return arguments.length;
+// }
 
-//const copy = Object.assign({}, orig);
-//const copy = JSON.parse(JSON.stringify(orig));
-const copy = orig.slice();
+// function sum(...thisArgs) {
+//         return arguments.reduce();
+// }
 
-//copy.test = 'newTest';
-//copy.userDetails.name = 'newName';
-//copy.push('guava');
-copy[2].name = 'coconut';
+//console.log(sum(5, 5, 5));
 
-console.log('orig:', orig);
+// const userDetails = {
+//     address: 'Anywhere',
+//     sample: {
+//         test: 'test'
+//     }
+// }
 
-console.log('copy:', copy);
+// const { name: newName = 'May', address, ...otherDetails } = userDetails;
+
+// console.log(newName);
+// console.log(otherDetails);
+
+// //const newUserDetails = { ...userDetails, mobileNumber: 123 }
+// const newUserDetails = { ...userDetails, name: 'May' }
+// //newUserDetails.sample.test = 'newTest';
+
+// console.log('userDetails:', userDetails);
+// console.log('newUserDetails:', newUserDetails);
+
+const fruits = [ undefined, 'guava', 'coconut']
+
+const [  firstFruit = 'strawberry', ...otherFruits ] = fruits;
+
+console.log(firstFruit);
+console.log(otherFruits);
+
+// console.log('userDetails:', fruits);
+// console.log('newUserDetails:', newFruits);

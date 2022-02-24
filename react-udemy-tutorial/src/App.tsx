ce8f16f9914c8fc3d8a0ed6e29b5facf4017bc24
Juan Aguilar <juan-carlos.aguilar@atos.net>
2022-02-24 16:05:24 -0600

    List and Keys Hands on

--- react-udemy-tutorial/src/App.tsx
+++ react-udemy-tutorial/src/App.tsx
@@ -1,16 +1,13 @@
 import React, {ErrorInfo} from 'react';
 import './App.css';
-import MyPurecomponent from './components/MyPureComponent';
-import NewsFeed from './components/NewsFeed';
-import  Profile from './components/Profile';
-import TrackClick from './components/TrackClick';
+import Fruits from './components/Fruits';
+
 
 export const App: React.FC = () => {
   return (
     <React.Fragment>
       <h1>My App</h1>
-      <Profile ownerName='Carlos' />
-      <NewsFeed />
+      <Fruits />
     </React.Fragment>
   );
 }

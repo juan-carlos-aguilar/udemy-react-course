59b1348b4dd54231cfc87a263a50f308905c9dd7
Juan Aguilar <juan-carlos.aguilar@atos.net>
2022-02-25 13:37:04 -0600

    Lesson 53 - Context API - Hands on

--- react-udemy-tutorial/src/App.tsx
+++ react-udemy-tutorial/src/App.tsx
@@ -1,38 +1,17 @@
 import React, {ErrorInfo} from 'react';
 import './App.css';
-import { Input } from './components/Input';
-import SuperComponent from './components/SuperComponent';
+import HomePage from './components/Homepage';
+import { MyContext, MyContextProvider } from './context/MyContext';
 
 
 
-class App extends React.Component {
-  inputRef: React.RefObject<HTMLInputElement>;
-
-  constructor(props: {}) {
-    super(props);
-
-    this.inputRef = React.createRef();
-  }
-
-  componentDidMount() {
-    console.log(this.inputRef.current);
-    this.inputRef.current && this.inputRef.current.focus();
-  }
-
-  // Callback ref example
-  // getInputRef = (element: HTMLInputElement) => {
-  //   this.inputRef = element;
-  // }
-
-  render() {
-    return (
-      <React.Fragment>
-        <h1>My App</h1>
-        <SuperComponent />
-        <Input ref={this.inputRef} />
-      </React.Fragment>
-    );
-  }
+function App() {
+  return (
+    <MyContextProvider>
+      <HomePage />
+    </MyContextProvider>
+  );
 }
+
 
 export default App;

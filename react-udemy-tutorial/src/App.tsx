bf75da974fb62d438f3d629a3049c55748d0d776
Juan Aguilar <juan-carlos.aguilar@atos.net>
2022-03-03 12:57:53 -0600

    Link and Redirect - Hands on

--- react-udemy-tutorial/src/App.tsx
+++ react-udemy-tutorial/src/App.tsx
@@ -2,18 +2,27 @@ import React from 'react';
 import './App.css';
 import HomePage from './components/Homepage';
 import AboutPage from './components/AboutPage';
-import { BrowserRouter, HashRouter, Route, Routes } from 'react-router-dom';
+import { BrowserRouter, HashRouter, Link, Navigate, Route, Routes } from 'react-router-dom';
+import ProfilePage from './components/ProfilePage';
+import { SpecialLink } from './components/SpecialLink';
 
 function App() {
   return (
     // for BrowserRouter normal path
     // for HashRouter path => '/#/_path_'
-      <HashRouter>
+      <BrowserRouter>
+      <Link to="/">Home</Link>
+      <br/>
+      <Link to="/about">About</Link>
+      <br/>
+      <Link to="/profile">Profile</Link>
         <Routes>
           <Route path="/about" element={<AboutPage />} />
           <Route path="/" element={<HomePage />} />
+          <Route path="/profile" element={<ProfilePage />} />
+          <Navigate to ="/home" />
         </Routes>
-      </HashRouter>
+      </BrowserRouter>
   );
 }
 

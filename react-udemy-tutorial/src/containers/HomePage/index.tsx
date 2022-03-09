923741e6ea9202847e1209b0b6504f6ec5b1a469
Juan Aguilar <juan-carlos.aguilar@atos.net>
2022-03-09 16:57:20 -0600

    Added some CSS to page

--- react-udemy-tutorial/src/containers/HomePage/index.tsx
+++ react-udemy-tutorial/src/containers/HomePage/index.tsx
@@ -1,10 +1,11 @@
 import React from "react";
+import './style.css';
 
 class HomePage extends React.Component {
     render() {
         return (
-            <div>
-                <h1>HomePage</h1>
+            <div className="homepage-container">
+                <div className="cover-image"/>
             </div>
         )
     }

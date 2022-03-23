1298f6e8debf9144291187d1a5f91c7a70101ae9
Juan Aguilar <juan-carlos.aguilar@atos.net>
2022-03-23 12:57:18 -0500

    useLayouitEffect v useEffect

--- react-udemy-tutorial/src/components/SuperComponent/index.tsx
+++ react-udemy-tutorial/src/components/SuperComponent/index.tsx
@@ -2,39 +2,10 @@ import React, { useEffect, useState } from "react";
 
 const SuperComponent: React.FC = () => {
     const [ counter, setCounter ] = useState(0);
-    const [ otherValue, setOtherValue ] = useState(0);
     
-    // Increase Counter Handle function
     const handleButtonClick = () => {
-        setOtherValue(otherValue + 1);
-
-        // To use 3 times
-        // setCounter(prevstate + 1);
+        setCounter(counter + 1);
     }
-
-    // Two params 2nd one is optional
-    useEffect(() => {
-        console.log('useEffect triggered');
-    });
-    
-    useEffect(() => {
-        console.log('Component Didmount');
-    }, [])
-    
-    useEffect(() => {
-        console.log('OtherValue');
-    }, [otherValue])
-    
-    useEffect(() => {
-        window.addEventListener('click', handleButtonClick);
-
-        return function cleanup() {
-            window.removeEventListener('click', handleButtonClick);
-        }
-    })
-
-    // Just to check console logs
-    console.log('render');
 
     return (
         <div className="App">

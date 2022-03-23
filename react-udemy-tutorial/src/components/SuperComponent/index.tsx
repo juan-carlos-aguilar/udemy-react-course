840f8e86891d73ea5dd48513b3a2f2d88e48b0be
Juan Aguilar <juan-carlos.aguilar@atos.net>
2022-03-23 10:02:25 -0500

    Intro to useState hook

--- react-udemy-tutorial/src/components/SuperComponent/index.tsx
+++ react-udemy-tutorial/src/components/SuperComponent/index.tsx
@@ -6,7 +6,12 @@ const SuperComponent: React.FC = () => {
     // Increase Counter Handle function
     const handleIncreaseCounter = () => {
         setCounter(counter + 1);
+        // To change 3 times
+        // setCounter(prevstate + 1);
     }
+
+    // Just to check console logs
+    console.log('render');
 
     return (
         <div className="App">

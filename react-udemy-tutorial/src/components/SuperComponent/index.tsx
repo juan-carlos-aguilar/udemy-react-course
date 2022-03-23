42c2934ba1b102177dae492744b9414b62947011
Juan Aguilar <juan-carlos.aguilar@atos.net>
2022-03-23 13:17:10 -0500

    Knowing differences between useEffect and LayoutEffect

--- react-udemy-tutorial/src/components/SuperComponent/index.tsx
+++ react-udemy-tutorial/src/components/SuperComponent/index.tsx
@@ -1,16 +1,45 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useLayoutEffect, useState } from "react";
 
 const SuperComponent: React.FC = () => {
     const [ counter, setCounter ] = useState(0);
+    const [ newValue, setNewValue ] = useState(0);
     
     const handleButtonClick = () => {
         setCounter(counter + 1);
     }
 
+    const handleNewValueButtonClick = () => {
+        setNewValue(newValue + 1);
+    }
+
+    
+
+    /**
+     * It runs after React paints the changes in the DOM
+     * Runs asynchronousy - non blocking
+     */
+    // useEffect(() => {
+    //         if(counter === 0) {
+    //             setCounter(5);
+    //         }
+    // }, [ counter, ]);
+    
+    /**
+     * Runs before React paints the changes in the DOM
+     * Runs Synchronous - blocking
+     * Updates scheduledi nside will be flushed synchronously
+     */
+    useLayoutEffect(() => {
+        if(counter === 0) {
+            setCounter(5);
+        }
+    }, []);
+
     return (
         <div className="App">
             <p>Counter: {counter}</p>
             <button onClick={handleButtonClick}>Add Value</button>
+            <button onClick={handleNewValueButtonClick}>Add Value</button>
         </div>
     )
 }

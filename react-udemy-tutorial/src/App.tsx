06236fe3dd1790dd75f62383743b337ce263a292
Juan Aguilar <juan-carlos.aguilar@atos.net>
2022-03-23 17:50:04 -0500

    useContext hook to access our context

--- react-udemy-tutorial/src/App.tsx
+++ react-udemy-tutorial/src/App.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import './App.css';
-import ClassComponent from './components/ClassComponent';
-import SuperComponent from './components/SuperComponent';
+import { SuperComponent } from './components/SuperComponent';
 
 function App() {
   return (
--- react-udemy-tutorial/src/components/SuperComponent/index.tsx
+++ react-udemy-tutorial/src/components/SuperComponent/index.tsx
@@ -1,28 +1,12 @@
-import React, { useRef, useCallback, useEffect, useLayoutEffect, useState } from "react";
-import { useEffectOnUpdates } from "../../customHooks/useEffectOnUpdate";
-import { usePrevious } from "../../customHooks/usePrevious";
+import React, { useContext } from "react";
+import { ThemeContext } from "../../context/ThemeContext";
 
-const SuperComponent: React.FC = () => {
-    const [ counter, setCounter ] = useState(0);
-    const previousValue = usePrevious(counter);
-    
-    const handleButtonClick = () => {
-        setCounter(counter + 1);
-    }
-
-    useEffectOnUpdates(() => {
-        console.log('Previous Value:', previousValue);
-        console.log('Current Value:', counter);
-        console.log('Component Did Update');
-    })
-
+export const SuperComponent: React.FC = () => {
+    const theme = useContext(ThemeContext);
     return (
-        <div className="App">
+        <div>
             <h1>Super Component</h1>
-            <p>Counter: {counter}</p>
-            <button onClick={handleButtonClick}>Update Counter</button>
+            <p>Theme: {theme}</p>
         </div>
-    )
+    );
 }
-
-export default SuperComponent;
